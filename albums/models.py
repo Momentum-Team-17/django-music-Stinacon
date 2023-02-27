2cbf917996129cfa94dbc251ad770bd3016c6793
Christina <stinacon@gmail.com>
2023-02-27 14:55:51 -0500

    added and registered Album model

--- albums/models.py
+++ albums/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Album(models.Model):
+    # Our Album class is inheriting from Django's Model class located in Django's models directory
     title = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
     genre = models.CharField(max_length=200)
